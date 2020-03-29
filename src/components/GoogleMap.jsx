19566ffb93478e33e16cd6ee82834091d91bf38c
VITALIY1986 <staslego1986@gmail.com>
2020-03-29 22:25:44 +0300

    made some

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -3,26 +3,32 @@ import GoogleMapReact from 'google-map-react'
 
 const defaultProps = {
   center: {
-    lat: 59.95,
-    lng: 30.33,
+    lat: 48.291888,
+    lng: 25.940761,
   },
-  zoom: 11,
+  zoom: 17,
 }
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>
-
+const AnyReactComponent = ({ text }) => <div className="map" style={{ height: '20px', width: '20px' ,     background:'#7f7f7f',    borderRadius: '1rem'
+}}>{text}</div>
+const AyReactComponent = ({ text }) => <div  className="map" style={{ height: '20px', width: '20px' ,     background:'#7f7f7f',   borderRadius: '1rem'}}>{text}</div>
 const GoogleMap = () => (
-  <div style={{ height: '100vh', width: '100%' }}>
+  <div style={{ height: '60vh', width: '100%' }}>
     <GoogleMapReact
       // bootstrapURLKeys={{ key: 'AlzaSyBRKoTpCZsUXfcXyxHxoK-PpXMHYwfqs8o' }}
-      bootstrapURLKeys={{ key: 'AlzaSyBmoLAJR2oWhUWjsEETeLRjjPtTlcOsJmXo' }}
+      bootstrapURLKeys={{ key: 'AIzaSyD19ds72niNKqjb1LPXDpJNaqL4lmnYjHg' }}
       defaultCenter={defaultProps.center}
       defaultZoom={defaultProps.zoom}
     >
       <AnyReactComponent
-        lat={59.955413}
-        lng={30.337844}
-        text={'Kreyser Avrora'}
+        lat={48.291888}
+        lng={25.940761}
+        text={' Avsr'}
+      />
+       <AyReactComponent
+        lat={48.291888}
+        lng={25.940000}
+        text={' Avsr'}
       />
     </GoogleMapReact>
   </div>
