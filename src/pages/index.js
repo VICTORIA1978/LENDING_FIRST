502b3db091bb711b63910c59375af1ffc182f780
VITALIY1986 <staslego1986@gmail.com>
2020-03-31 22:00:34 +0300

    mad

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import GoogleMap from '../components/GoogleMap'
 
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {faInstagram,faFacebookF,faYoutube,faGoogle } from "@fortawesome/free-brands-svg-icons"
 
 
 
@@ -20,8 +22,13 @@ export default  () => {
     <Row className="">
       <Col className="container_overlay_inner  p-0  vh-100     position-relative ">
      
-     
-                
+     <div className="mod_social_block d-flex flex-column mt-3 mr-2  position-fixed">
+     <span className="social  mb-2 d-flex align-items-center justify-content-center text-light "> <FontAwesomeIcon icon={faInstagram} /></span>
+     <span className="social mb-2 d-flex align-items-center justify-content-center text-light"><FontAwesomeIcon icon={faFacebookF} /></span>
+     <span className="social mb-2 d-flex align-items-center justify-content-center text-light"><FontAwesomeIcon icon={faYoutube} /></span>
+     <span className="social mb-2 d-flex align-items-center justify-content-center text-light"><FontAwesomeIcon icon={faGoogle} /></span>
+     </div>
+
       </Col>
       <div className=" style vh-100  w-100 position-absolute "></div>
       </Row>
@@ -97,7 +104,7 @@ export default  () => {
           с помощью платья идеально отразить личность[]
            каждой невесты.</p>
           
-           <GoogleMap /> 
+         
        </Col>
        </Row>
      </div>
@@ -150,7 +157,7 @@ export default  () => {
       <div className="overlay_fixed position-absolute">
         <div className="d-flex justify-content-center align-items-center h-100">
         <Container  fluid className="  ">
-          <h3 className="d-flex justify-content-center">СОТРУДНИЧЕСТВО</h3>
+          <h3 className="d-flex justify-content-center text-light">СОТРУДНИЧЕСТВО</h3>
           <div className="d-flex justify-content-center"><Modal/></div>
          </Container>
 
@@ -179,26 +186,17 @@ export default  () => {
         <Carousel>
   <Carousel.Item>
     <div className="der"></div>
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
+   
   </Carousel.Item>
   <Carousel.Item>
         <div className="der"></div>
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
+    
   </Carousel.Item>
   <Carousel.Item>
   <div className="der"></div>
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
+   
   </Carousel.Item>
 </Carousel>
         
@@ -228,8 +226,8 @@ export default  () => {
       <div className="overlay_fixed position-absolute">
         <div className="d-flex justify-content-center align-items-center h-100">
         <Container   className="  ">
-          <h3 className="d-flex justify-content-center">СОТРУДНИЧЕСТВО</h3>
-          <p className="d-flex justify-content-center text-center">Доставку разрабатываем совместно, исходя из вашего 
+          <h3 className="d-flex justify-content-center text-light">СОТРУДНИЧЕСТВО</h3>
+          <p className="d-flex justify-content-center text-center text-light">Доставку разрабатываем совместно, исходя из вашего 
 желания и местонахождения.
 По предварительной договорённости товар может 
 быть доставлен в любую точку Украины, СНГ или Европы.</p>
