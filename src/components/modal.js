30feb89009a82bae02a09556e505805f7d13bde1
VITALIY1986 <staslego1986@gmail.com>
2020-04-01 16:12:06 +0300

    dffdf

--- src/components/modal.js
+++ src/components/modal.js
@@ -122,22 +122,13 @@ const Navbar = class extends React.Component {
         </li></ul>
       </div>
       <div class="modal-footer">
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
+ 
+  
+
   <p>
     <button type="submit">Send</button>
   </p>
