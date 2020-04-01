ca7fe8715a88a1e145cdc15ab38a7c120561918b
VITALIY1986 <staslego1986@gmail.com>
2020-04-01 17:29:51 +0300

    sss

--- src/components/form.js
+++ src/components/form.js
@@ -2,13 +2,13 @@ import React from "react"
 const IndexPage = () =>(
 
 <form 
-name="contact"
+name=" contact "
  method="post" 
  data-netlify="true" 
  data-netlify-honeypot="bot-field">
  
-  <input type="text" placeholder="you"  />
-<button>sdsdsd</button>
+  <input name="name" type="text" placeholder="you"  />
+<button>send</button>
 </form>
 
 
