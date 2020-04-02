41fb474d3adb167840d7082a404df43f3be393e9
VITALIY1986 <staslego1986@gmail.com>
2020-04-02 19:39:04 +0300

    adsfaf

--- src/pages/index.js
+++ src/pages/index.js
@@ -139,7 +139,7 @@ export default  () => {
       
       
       <div class="embed-responsive embed-responsive-16by9">
-<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/TKZ0QoHuRUk" allowfullscreen></iframe>
 </div>
         
      
